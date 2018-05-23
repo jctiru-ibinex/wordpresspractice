57f8f15c12b5a1ca37ed31ac183ca84126880df0
jctiru-ibinex <jonathan.ti@ibinex.tech>
2018-05-23 10:21:24 +0800

    Added custom post type

--- wp-content/themes/awesometheme/functions.php
+++ wp-content/themes/awesometheme/functions.php
@@ -71,3 +71,42 @@ require_once get_template_directory() . './inc/class-wp-bootstrap-navwalker.php'
 
 // Pagination Helper
 require_once get_template_directory() . './inc/pagination.php';
+
+// Custom Post Type
+function awesome_custom_post_type(){
+	$labels = [
+		'name' => 'Portfolio',
+		'singular_name' => 'Portfolio',
+		'add_new' => 'Add Item',
+		'all_items' => 'All Items',
+		'add_new_item' => 'Add Item',
+		'edit_item' => 'Edit Item',
+		'new_item' => 'New Item',
+		'view_item' => 'View Item',
+		'search_item' => 'Search Portfolio',
+		'not_found' => 'No items found',
+		'not_found_in_trash' => 'No items found in trash',
+		'parent_item_colon' => 'Parent Item',
+	];
+	$args = [
+		'labels' => $labels,
+		'public' => true,
+		'has_archive' => true,
+		'publicly_queryable' => true,
+		'query_var' => true,
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'supports' => ['title', 'editor', 'excerpt', 'thumbnail', 'revisions'],
+		'taxonomies' => ['category', 'post_tag'],
+		'menu_position' => 5,
+		'exclude_from_search' => false,
+		'menu_icon' => 'dashicons-id-alt'
+	];
+	register_post_type('portfolio', $args);
+}
+add_action('init', 'awesome_custom_post_type');
+
+/* Flush rewrite rules for custom post types. */
+add_action( 'after_switch_theme', 'flush_rewrite_rules' );
+add_action('switch_theme', 'flush_rewrite_rules');
